777e0052bf137f87ccda2ab4fb7a64206d4cc475
austl001 <56455653+austl001@users.noreply.github.com>
2021-12-31 14:09:37 +0000

    corrected anchor error

--- search-website/src/components/Results/Result/Result.js
+++ search-website/src/components/Results/Result/Result.js
@@ -10,8 +10,9 @@ export default function Result(props) {
     <div className="card result">
         <div className="card-body">
             <h5 className="title-style">
-                <a href={`/details/${props.document.id}`}></a>
+                <a href={`/details/${props.document.id}`}>
                 {props.document.metadata_storage_name}
+                </a>
             </h5>
             <h6 className="card-text">Search Score: {props.score.toLocaleString()}</h6>
             <h6 className="card-text">{props.document.keyPhrases?.join(' | ')}</h6>
